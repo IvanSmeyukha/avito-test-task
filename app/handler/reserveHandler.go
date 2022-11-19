4e62f24ad25eb3598fdc33a4bc99996b89b9beee
BAHOXA <ivan.smeiukha@yandex.ru>
2022-11-19 19:46:38 +0300

    init

--- app/handler/reserveHandler.go
+++ app/handler/reserveHandler.go
@@ -7,18 +7,6 @@ import (
 	"strconv"
 )
 
-// @Summary SignUp
-// @Tags auth
-// @Description create account
-// @ID create-account
-// @Accept  json
-// @Produce  json
-// @Param input body todo.User true "account info"
-// @Success 200 {integer} integer 1
-// @Failure 400,404 {object} errorResponse
-// @Failure 500 {object} errorResponse
-// @Failure default {object} errorResponse
-// @Router /auth/sign-up [post]
 func (h *Handler) reserveMoneyFromUserBalance(c *gin.Context) {
 	id, _ := strconv.Atoi(c.Param("id"))
 	var trans models.Transaction
